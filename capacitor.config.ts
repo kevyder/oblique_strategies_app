a1fa6d4733617b309a16aea2016334d88fd8634d
Kevin Rodríguez <kyrodriguez97@gmail.com>
2025-07-24 17:31:36 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'oblique_strategies_app',
+  webDir: 'dist'
+};
+
+export default config;
